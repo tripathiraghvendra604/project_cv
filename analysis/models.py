e40e0880e035e9bd8063e8f66ffa98e4f578fc54
raghvendra tripathi <tripathiraghvendra604@gmail.com>
2016-06-08 18:26:06 +0530

    added user info models

--- analysis/models.py
+++ analysis/models.py
@@ -7,6 +7,14 @@ from django.contrib.auth.models import User
 
 class UserInfo(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL)
-    name = models.CharField(max_length=100)
-    dob = models.DateField()
-    image = models.ImageField
+    fname = models.CharField(max_length=100)
+    lname = models.CharField(max_length=100)
+    dob = models.DateField(auto_now_add=False, auto_now=False)
+    place_of_birth = models.CharField(max_length=100)
+    image = models.ImageField(null=False,
+                              blank=False,
+                              width_field='width_field',
+                              height_field='height_field'
+                              )
+    height_field = models.IntegerField(default=0)
+    width_field = models.IntegerField(default=0)
