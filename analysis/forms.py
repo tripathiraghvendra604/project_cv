dc1ca0f6dc2d0ea47490c93fb0e34150163ea797
raghvendra tripathi <tripathiraghvendra604@gmail.com>
2016-06-08 19:07:36 +0530

    select date of birth from calender

--- analysis/forms.py
+++ analysis/forms.py
@@ -1,6 +1,11 @@
 from django import forms
 from .models import UserInfo
 
+
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
 class UserInfoForm(forms.ModelForm):
     class Meta:
         model = UserInfo
@@ -11,3 +16,6 @@ class UserInfoForm(forms.ModelForm):
             'place_of_birth',
             'image',
         ]
+        widgets = {
+            'dob': DateInput(),
+        }
